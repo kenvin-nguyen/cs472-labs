964ce497ff4bdd4bd0c9fa8ec77113ee3739d872
kevin nguyen <ndkhanh99.job@gmail.com>
2020-03-15 23:26:51 -0600

    Update fifteen.js

--- js/fifteen.js
+++ js/fifteen.js
@@ -1,141 +1,150 @@
 "use strict";
 $(document).ready(function(){
-	init();
+	module.init();
     $("#shufflebutton").bind('click', function () {
-        shufflingTile();
+        module.shufflingTile();
     });
 });
 
-let emptyBlock = {};
-let tileRow = 3;
-let tileCol = 3;
-const noOfRows_Cols = 4;
-const tileSides = 100;
-function init(){
-	$('#puzzlearea > div').each(function(idx, e){
-		let x = ((idx % noOfRows_Cols) * tileSides) ;
-		let y = (Math.floor(idx / noOfRows_Cols) * tileSides) ;
-		let tile = $(this);
+let module = (function(){
+	let emptyBlock = {};
+	let tileRow = 3;
+	let tileCol = 3;
+	const noOfRows_Cols = 4;
+	const tileSides = 100;
+	function init(){
+		$('#puzzlearea > div').each(function(idx){
+			let x = ((idx % noOfRows_Cols) * tileSides) ;
+			let y = (Math.floor(idx / noOfRows_Cols) * tileSides) ;
+			let tile = $(this);
 
-		// set basic style and background
-		tile.addClass("puzzlepiece");
-		tile.css({
-			'left': x + 'px',
-			'top': y + 'px',
-			'background-image': 'url("images/background-puzzle.jpg")',
-			'background-position-x': -x + 'px',
-			'background-position-y': (-y) + 'px',
+			// set basic style and background
+			tile.addClass("puzzlepiece");
+			tile.css({
+				'left': x + 'px',
+				'top': y + 'px',
+				'background-image': 'url("images/background-puzzle.jpg")',
+				'background-position-x': -x + 'px',
+				'background-position-y': (-y) + 'px',
+			});
+		
+			tile.innerHTML = x + "/" + y;
+			
+			// store x and y for later 
+			tile.data('x', x);
+			tile.data('y', y);
+			
+			setTileId(tile, x, y);
+			
+			tile.bind('click', function(){
+				if (checkBlockCanMove(tile)) {
+                    shiftTile(tile);
+                }
+				
+			});
+			
+			// tile 15th is blank at initial
+			if (idx === 14) {
+				emptyBlock = {x: x + tileSides, y: y};
+				setBlockCanMove();
+			}
+		
 		});
-	
-        tile.innerHTML = x + "/" + y;
-		
-		// store x and y for later 
-		tile.data('x', x);
-		tile.data('y', y);
-		
-		setTileId(tile, x, y);
-		
-		tile.bind('click', function(){
-			shiftTile(tile);
-		});
-		
-		// tile 15th is blank at initial
-		if (idx === 14) {
-			emptyBlock = {x: x + tileSides, y: y};
+	}
+
+	function isSameRowAndCol(tile) {
+		return tile.data('x') === emptyBlock.x || tile.data('y') === emptyBlock.y;
+	}
+
+	function setTileId(tile, x, y) {
+		tileRow = (x / tileSides) + 1;
+		tileCol = (y / tileSides) + 1;
+		tile.data('id', 'square_' + tileRow + "_" + tileCol);
+	}
+
+	function shiftTile(tile){
+		let currentX = tile.data('x');
+		let currentY = tile.data('y');
+		if (checkBlockCanMove(tile)) {
+			tile.animate({left: emptyBlock.x + 'px', top: emptyBlock.y + 'px'});
+			tile.data('x', emptyBlock.x);
+			tile.data('y', emptyBlock.y);
+			emptyBlock.x = currentX;
+			emptyBlock.y = currentY;
 			setBlockCanMove();
 		}
-	
-	});
-}
-
-function isSameRowAndCol(tile) {
-	return tile.data('x') === emptyBlock.x || tile.data('y') === emptyBlock.y;
-}
-
-function setTileId(tile, x, y) {
-	tileRow = (x / tileSides) + 1;
-	tileCol = (y / tileSides) + 1;
-	tile.data('id', 'square_' + tileRow + "_" + tileCol);
-}
-
-function shiftTile(tile){
-	//alert('shift');
-	let currentX = tile.data('x');
-	let currentY = tile.data('y');
-	if (checkBlockCanMove(tile)) {
-		tile.animate({left: emptyBlock.x + 'px', top: emptyBlock.y + 'px'});
-		tile.data('x', emptyBlock.x);
-		tile.data('y', emptyBlock.y);
-		emptyBlock.x = currentX;
-		emptyBlock.y = currentY;
-		setBlockCanMove();
 	}
-}
 
-function checkBlockCanMove(tile) {
-	//console.log("checkBlockCanMove tile:" + tile.data('x') + "/" + tile.data('y') + "-" + emptyBlock.x + "/" + emptyBlock.y);
-	let blockWidth = 100;
-	if (!isSameRowAndCol(tile)) {
-		console.log("No no no" + tile.data('id'));
-		return false;
-	}
-	
-	if (tile.data('x') + blockWidth === emptyBlock.x 
-		|| tile.data('x') - blockWidth === emptyBlock.x 
-		|| tile.data('y') + blockWidth === emptyBlock.y 
-		|| tile.data('y') - blockWidth === emptyBlock.y){
-		//console.log("moveable tile: " + tile.data('id'));
-		return true;
-	} else {
-		console.log("no no no" + tile.data('id'));
-		return false;
-	}	
-}
-
-function setBlockCanMove() {
-	let tiles = $("#puzzlearea > div");
-	tiles.each(function (i) {
-		let tile = $(this);
-		if (checkBlockCanMove(tile)) {
-			console.log("true " + tile.data('id'));
-				tile.addClass("movablepiece");
-				tile.data('moveable',true);
-		} else {
-			tile.removeClass("movablepiece");
-			tile.data('moveable',false);
+	function checkBlockCanMove(tile) {
+		let tileWidth = 100;
+		if (!isSameRowAndCol(tile)) {
+			return false;
 		}
-	});
-}
+		
+		if (tile.data('x') + tileWidth === emptyBlock.x 
+			|| tile.data('x') - tileWidth === emptyBlock.x 
+			|| tile.data('y') + tileWidth === emptyBlock.y 
+			|| tile.data('y') - tileWidth === emptyBlock.y){
+			
+			return true;
+		} else {
+			return false;
+		}	
+	}
 
-function moveTile(div) {
-    let currentX = div.data('x');
-    let currentY = div.data('y');
-    if (checkBlockCanMove(div)) {
-        div.animate({left: emptyBlock.x + 'px', top: emptyBlock.y + 'px'});
-        div.data('x', emptyBlock.x);
-        div.data('y', emptyBlock.y);
-        emptyBlock.x = currentX;
-        emptyBlock.y = currentY;
-        setBlockCanMove();
-    }
-}
+	function setBlockCanMove() {
+		let tiles = $("#puzzlearea > div");
+		tiles.each(function (i) {
+			let tile = $(this);
+			if (checkBlockCanMove(tile)) {
+					tile.addClass("movablepiece");
+					tile.data('moveable',true);
+			} else {
+				tile.removeClass("movablepiece");
+				tile.data('moveable',false);
+			}
+		});
+	}
 
-function shufflingTile() {
-    var lastMove;
-    function moveRandom() {
-		let listTile = [];
-        $("#puzzlearea > .movablepiece").each(function (index) {
-            if ($(this).data('moveable')) {
-                listTile.push($(this));
+	function moveTile(tile, callback) {
+		let currentX = tile.data('x');
+		let currentY = tile.data('y');
+		let id = tile.data("id");
+		tile.animate({left: emptyBlock.x + "px", top: emptyBlock.y + "px"}, {
+            duration: 10,
+            complete: function () {
+                tile.data("x", emptyBlock.x);
+                tile.data("y", emptyBlock.y);
+                tile.data("id", id);
+                emptyBlock.x = currentX;
+                emptyBlock.y = currentY;
+                emptyBlock.id = id;
+                setBlockCanMove();
+                if (callback) {
+                    callback();
+                }
             }
         });
-        var randompick = $(listTile[Math.floor(Math.random() * listTile.length)]);
-        if (randompick === lastMove) {
-            return moveRandom();
-        }
-        lastMove = randompick;
-        moveTile(randompick);
-    }
-    moveRandom();
-}
+	}
 
+	let i = 0;
+	let lastMove;
+	function shufflingTile() {
+		function moveRandom() {
+            i += 1;
+            let divs = $("#puzzlearea > .movablepiece");
+            let randompick = $(divs[Math.floor(Math.random() * divs.length)]);
+            if (randompick === lastMove) {
+                return moveRandom();
+            }
+            lastMove = randompick;
+            if (i < 40) {
+                moveTile(randompick, shufflingTile);
+            } else {
+                i = 0;
+            }
+        }
+        moveRandom();
+	}
+	return{init, shufflingTile};
+})();
